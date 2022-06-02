bcb4b8171f07447fcd32becf8b6b440897298430
qinran0423 <1156107187@qq.com>
2022-06-02 10:56:06 +0800

    day1

---
+++ day1/index.js
@@ -0,0 +1,24 @@
+const aa = 21
+const bb = 222222
+
+
+const cc = 21
+const cd = 111112222222444444
+
+
+const nnnnnn = 111111222222111
+const mmmmmmmm = 2222222
+
+const obj = {
+  aa,
+  cc
+}
+
+function add (a, b) {
+  return a + b
+}
+
+
+add(aa, bb)
+
+add(cc, dd)
---
+++ day1/vim-day.md
@@ -0,0 +1,13 @@
+### 第一天
+
+jfjj dfkjsdfkls dfjklsdfjs jdklfjsdlfkj lsdjflsdjfsdfk jdjfsjdflsdjf 
+jldfjlsf jflkjsfl kd jslfkjsdlfk jsd jldfjsd f
+### 练习
+
+
+```js 
+const aa = 11
+const bb = 22
+
+
+```
