485bc7199a72360667dd2a6d4c053e5ab8a0089e
qinran0423 <1156107187@qq.com>
2022-06-03 15:23:36 +0800

    day2

---
+++ day2/index.js
@@ -0,0 +1,17 @@
+function serializeElement(
+  node,
+  indent,
+  depth
+){
+  const props = Object.keys(node.props)
+  .map(key => {
+    const value = node.props[key]
+    return isOn(key) || value == null
+      ? ''
+      : value === ''
+      ? key
+      : `${key}=${JSON.stringify(value)}`
+  })
+  .filter(Boolean)
+  .join(" ")
+}
