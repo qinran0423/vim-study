ea03b8527fccf05638bebc3fed98e0aff48a0394
qinran0423 <1156107187@qq.com>
2022-06-22 22:37:39 +0800

    day 18

---
+++ day18/index.js
@@ -0,0 +1,27 @@
+
+function haha(argument) {
+  const heihei = 111
+  const b = 222
+  if (a === b) {
+
+
+
+
+
+
+
+
+
+
+    console.log(heihei + b);
+
+
+    console.log("hahah");
+
+  }
+}
+
+
+function test() {
+  console.log('test');
+} 
---
+++ day18/vim18.md
@@ -0,0 +1,11 @@
+## vim 调用vscode的命令
+
+## 格式化文档
+   -  vscode -> shift + alt + f
+   -  vim -> <Leader> + f + d
+## 重命名
+   -  vscode -> f2
+   -  vim - <Leader> + r + n
+## 折叠代码
+   - vscode -> alt + cmd + [
+   - vim -> <Leader> + [
